fb833125ba2b92ce737de3f598c8751b62fe2782
enessanal <enessanal@hotmail.com.tr>
2019-09-30 11:07:20 +0300

    Seperate Controllers and Routers

--- repo/Routers.py
+++ repo/Routers.py
@@ -1,4 +1,4 @@
-# Controllers.py BEGIN
+# Routers.py BEGIN
 #
 #
 #
@@ -15,9 +15,9 @@ if __name__ == "__main__":
 #
 #
 ###########################################################################
-# + Importing Models.py BEGIN
+# + Importing Controllers.py BEGIN
 ###########################################################################
-from Models import *
+from Controllers import *
 ###########################################################################
 # - Importing Models.py END
 ###########################################################################
@@ -25,73 +25,66 @@ from Models import *
 #
 #
 ###########################################################################
-# + loginController BEGIN
+# + Route Login BEGIN
 ###########################################################################
-def loginController(request):
-  title="Login"
-  parameters={"title":title}
-
-  if(request.method=="GET"):
-    if g.user:
-      return redirectRoute("panel")
-    return render_template("login.html",parameters=parameters)
-
-  elif(request.method=="POST"):
-    if g.user:
-      return redirectRoute("panel")
-    else:
-      if(request.form["username"]=="admin" and request.form["password"]=="123"):
-        session["user"] = request.form["username"]
-        return redirectRoute("panel")
-      else:
-        parameters["errors"]="error"
-        return render_template("login.html",parameters=parameters)
-  else:
-    abort(405)
+@app.route("/login",methods=["GET","POST"])
+def loginRoute():	
+  return loginController(request)
 ###########################################################################
-# - loginController END
+# - Route Login END
 ###########################################################################
 #
 #
 #
 ###########################################################################
-# + logoutController BEGIN
+# + Route Logout BEGIN
 ###########################################################################
-def logoutController(request):
- 
-  if(request.method=="POST"):
-    if g.user:
-      session.pop("user",None)
-    return redirectRoute("login")
-
-
-  else:
-    abort(405)
+@app.route("/logout",methods=["POST"])
+def logoutRoute():	
+  return logoutController(request)
 ###########################################################################
-# - logoutController END
+# - Route Logout END
 ###########################################################################
 #
 #
 #
 ###########################################################################
-# + panelController BEGIN
+# + Route Panel BEGIN
 ###########################################################################
-def panelController(request):
-  title="Panel"
-  parameters={"title":title}
-
-  if(request.method=="GET"):
-    if g.user:
-      parameters["username"]=g.user
-      return render_template("panel.html",parameters=parameters)
-
-    else:
-      return redirect(url_for("loginRoute"))
-  else:
-    abort(405)
+@app.route("/panel",methods=["GET"])
+def panelRoute():	
+  return panelController(request)
 ###########################################################################
-# - panelController END
+# - Route Panel END
 ###########################################################################
 #
 #
-# Controllers.py END
+#
+###########################################################################
+# + Route Home BEGIN
+###########################################################################
+@app.route("/",methods=["GET"])
+def homeRoute():	
+  return redirectRoute("login")
+###########################################################################
+# - Route Home END
+###########################################################################
+#
+#
+#
+###########################################################################
+# + App Before Request BEGIN
+###########################################################################
+@app.before_request
+def before_request():
+  print("----------------------------")
+  print("----------------------------")
+  g.user = None
+  if "user" in session:
+    g.user = session["user"]
+###########################################################################
+# - App Before Request END
+###########################################################################
+#
+#
+# Routers.py END
